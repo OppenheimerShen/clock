33160847a553b0adac1c34b43cbc91dfecad157c
oppemheimershen <sya_123123@outlook.com>
2022-06-01 09:20:07 +0800

    beta版的clock，还有许多功能尚未完善

--- ClockBeta.py
+++ ClockBeta.py
@@ -1,105 +1,141 @@
-import time
-import tkinter
-from tkinter import *
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@File    :  ClockBeta.py
+@Time    :  2022/5/31 08:03
+@Author  :  OppenheimerShen
+@Contact :  sya_123123@outlook.com
+@License :  (C)Copyright 2022-2023
+@version :  0.0.2
+@mainIdea:
+"""
 
-class clock:
-
-    def __init__(self) -> None:
-        self.root = tkinter.Tk()
-
-    def getTime(self):
-
-        timestr = time.strftime("%H:%M:%S") # 获取当前的时间并转化为字符串
-        self.lb.configure(text = timestr)
-        self.root.after(1000, self.getTime)
-
-    def countdownStarts(self):
-        allTimeSec=self.hour*3600+self.min*60+self.sec
-        while allTimeSec==0:
-            self.sec=self.sec-1
-            if self.sec==0:
-                self.min-1
-                self.sec=60
-            if self.min==0:
-                self.hour-1
-                self.min=60
-            text=f'{self.hour}:{self.min}:{self.sec}'
-            CountDownTime=time+allTimeSec.strftime("%H:M%:S%")
-            self.endCountDownTime=Label(text="{CountDownTime}")
-            self.endCountDownTIme.place(x=20, y=120)
-
-    def timeProcessing(self, time):
-
-        newTime=[]
-        for i in time:
-            i=int(i)
-            newTime.append(i)
-        B_hour=newTime[0:2]
-        B_min=newTime[2:4]
-        B_sec=newTime[4:6]
-
-        self.hour=B_hour[0]*10+B_hour[1]
-        self.min=B_min[0]*10+B_min[1]
-        self.sec=B_sec[0]*10+B_sec[1]
-
-        self.startOperation()
+import time  # 用于计时
+import tkinter  # 用于创建GUI界面
+from tkinter import *  # 用于创建各种控件
 
 
+class Clock:
+    """时间类"""
 
+    def __init__(self):
+        """初始化该类"""
+        self.stopwatch = None
+        self.timer_button = None
+        self.input_h = None
+        self.show_fact_time = None
 
-    def CountDown(self):
-        self.root.geometry("200x180")
-        self.enter_CountDown = tkinter.Entry()
-        self.enter_CountDown.place(x=5, y=90)
+        self.show_timer_sec=None
+        self.show_timer_min=None
+        self.show_timer_h=None
+        self.root = tkinter.Tk()  # 创建主屏幕
 
-        self.hint_CountDown = tkinter.Label(text="XXXXXX\n(h/min/s)",
-                    font=('', 15))
+    def get_time(self):
+        """用于获取实时时间"""
+        timestr = time.strftime("%H:%M:%S")  # 获取当前的时间并转化为z字符串
+        self.show_fact_time.configure(text=timestr)
+        self.root.after(1000, self.get_time)
 
-        self.hint_CountDown.place(x=12, y=120)
+    def timer(self):
+        """计时器"""
+        self.root.geometry("200x150")
 
-        self.OK_CountDown = tkinter.Button(text="OK",
-                    command=lambda:self.startOperation(self.timeProcessing(self.enter_CountDown.get())),
-                    width=5)
+        self.input_h = Entry()
+        self.input_h.place(x=10, y=90, height=30, width=40)
 
-        self.OK_CountDown.place(x=110, y=118)
-        self.Cancel_CountDown = tkinter.Button(text="Cancel", width=5)
-        self.Cancel_CountDown.place(x=110, y=145)
+        self.input_min = Entry()
+        self.input_min.place(x=50, y=90, height=30, width=40)
 
-    def startOperation(self):
+        self.input_s = Entry()
+        self.input_s.place(x=90, y=90, height=30, width=40)
 
+        self.typing_hint = Label(text="HOUR/MIN/SEC", fg="blue", font=("黑体", 15))
+        self.typing_hint.place(x=20, y=120)
 
-        self.enter_CountDown.destroy()
-        self.hint_CountDown.destroy()
-        self.OK_CountDown.destroy()
-        self.Cancel_CountDown.destroy()
-        self.root.geometry('200x140')
+        self.timer_ok = Button(text="OK", command=lambda: self._timer_ok())
+        self.timer_ok.place(x=140, y=90, height=30, width=50)
+        self.time_cancel = Button(text="Cancel", command=lambda: self._timer_cancel())
+        self.time_cancel.place(x=140, y=120, height=25, width=50)
 
-        self.hint_start_CountDown = Label(text=f"倒计时{self.hour}时{self.min}分{self.sec}秒")
-        self.hint_start_CountDown.place(x=30, y=115)
+    def _timer_ok(self):
+        """开始倒计时"""
 
+        self.timer_sec = int(self.input_s.get())
+        self.timer_min = int(self.input_min.get())
+        self.timer_h = int(self.input_h.get())
 
+        self.del_timer_choice()
 
+        self.show_timer_fact_time_all=Label(text="")
+        self.show_timer_fact_time_all.pack(side='top',fill='y',expand=True)
 
+        self._timer_ok_progress()
+
+    def _timer_ok_progress(self):
+        """倒计时处理区"""
+
+        self.timer_sec = self.timer_sec - 1
+        if self.timer_sec == 0 and self.timer_min != 0:
+            self.timer_sec = 60
+            self.timer_min = self.timer_min - 1
+        if self.timer_min == 0 and self.timer_h != 0:
+            self.timer_min = 60
+            self.timer_h = self.timer_h - 1
+        if self.timer_h == 0 and self.timer_min == 0 and self.timer_sec == 0:
+            pass
+
+        if self.timer_sec < 10:
+            self.show_timer_sec = f"0{self.timer_sec}"
+
+        if self.timer_min < 10:
+            self.show_timer_min = f"0{self.timer_min}"
+
+        if self.timer_h < 10:
+            self.show_timer_h = f"0{self.timer_h}"
+
+        self.show_timer_fact_time=f"{self.show_timer_h}:{self.show_timer_min}:{self.timer_h}"
+
+        self.show_timer_fact_time_all.configure(text=self.show_timer_fact_time)
+
+        self.root.after(1000, self._timer_ok_progress)
+
+    def _timer_cancel(self):
+        """在倒计时选择界面退出"""
+
+        # 删除timer下的控件并还原尺寸
+        self.root.geometry("200x100")
+        self.del_timer_choice()
+
+    def del_timer_choice(self):
+        """删除倒计时选择界面的控件"""
+        self.input_s.destroy()
+        self.input_min.destroy()
+        self.input_h.destroy()
+        self.typing_hint.destroy()
+        self.timer_ok.destroy()
+        self.time_cancel.destroy()
 
     def main(self):
+        """主程序"""
+        self.root.title("时钟")  # 设置主屏幕标题
+        self.root.geometry("200x100")  # 设置尺寸
+        self.root.resizable(False, False)  # 不可缩放
 
-        self.root.title("时钟")
-        self.root.geometry("200x100")
-        self.lb = tkinter.Label(text = '', fg = 'black', font = ('黑体', 40))
+        # 定义计时器按钮
+        self.timer_button = Button(text="计时器", command=lambda: self.timer())
+        self.timer_button.place(x=20, y=50, height=40, width=80)
+        # 定义秒表按钮
+        self.stopwatch = Button(text="秒表")
+        self.stopwatch.place(x=100, y=50, height=40, width=80)
 
-        timer = Button(self.root, text='计时器', command=lambda:self.CountDown())
-        timer.place(x=20, y=50, height=40, width=80)
+        self.show_fact_time = tkinter.Label(text="", fg="black", font=("黑体", 40))
+        # 初始化显示时间的标签
+        self.show_fact_time.pack()  # 使用pack函数封装
 
+        self.get_time()  # 调用更新时间函数
 
-        stopwatch = Button(self.root, text='秒表')
-        stopwatch.place(x=100, y=50, height=40, width=80)
+        self.root.mainloop()  # 进入循环
 
-        self.lb.pack()
-
-        self.getTime()
-
-        self.root.mainloop()
 
 if __name__ == "__main__":
-
-    clock().main()
+    Clock().main()
