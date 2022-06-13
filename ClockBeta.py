96137aef989a38c6d758b8661523530db553ed90
Oppenheimer-Shen <85938104+OppenheimerShen@users.noreply.github.com>
2022-06-13 11:01:29 +0800

    Clock正式版

--- ClockBeta.py
+++ ClockBeta.py
@@ -1,33 +1,15 @@
-# !/usr/bin/python
-# -*- coding: utf-8 -*-
-"""
-@File    :  ClockBeta.py
-@Time    :  2022/5/31 08:03
-@Author  :  OppenheimerShen
-@Contact :  sya_123123@outlook.com
-@License :  (C)Copyright 2022-2023
-@version :  0.0.2
-@mainIdea:
-"""
-
+from string import hexdigits
 import time  # 用于计时
 import tkinter  # 用于创建GUI界面
-from tkinter import *  # 用于创建各种控件
+from tkinter import *
 
+from pyrsistent import m  # 用于创建各种控件
 
 class Clock:
     """时间类"""
 
     def __init__(self):
         """初始化该类"""
-        self.stopwatch = None
-        self.timer_button = None
-        self.input_h = None
-        self.show_fact_time = None
-
-        self.show_timer_sec=None
-        self.show_timer_min=None
-        self.show_timer_h=None
         self.root = tkinter.Tk()  # 创建主屏幕
 
     def get_time(self):
@@ -36,96 +18,17 @@ class Clock:
         self.show_fact_time.configure(text=timestr)
         self.root.after(1000, self.get_time)
 
-    def timer(self):
-        """计时器"""
-        self.root.geometry("200x150")
-
-        self.input_h = Entry()
-        self.input_h.place(x=10, y=90, height=30, width=40)
-
-        self.input_min = Entry()
-        self.input_min.place(x=50, y=90, height=30, width=40)
-
-        self.input_s = Entry()
-        self.input_s.place(x=90, y=90, height=30, width=40)
-
-        self.typing_hint = Label(text="HOUR/MIN/SEC", fg="blue", font=("黑体", 15))
-        self.typing_hint.place(x=20, y=120)
-
-        self.timer_ok = Button(text="OK", command=lambda: self._timer_ok())
-        self.timer_ok.place(x=140, y=90, height=30, width=50)
-        self.time_cancel = Button(text="Cancel", command=lambda: self._timer_cancel())
-        self.time_cancel.place(x=140, y=120, height=25, width=50)
-
-    def _timer_ok(self):
-        """开始倒计时"""
-
-        self.timer_sec = int(self.input_s.get())
-        self.timer_min = int(self.input_min.get())
-        self.timer_h = int(self.input_h.get())
-
-        self.del_timer_choice()
-
-        self.show_timer_fact_time_all=Label(text="")
-        self.show_timer_fact_time_all.pack(side='top',fill='y',expand=True)
-
-        self._timer_ok_progress()
-
-    def _timer_ok_progress(self):
-        """倒计时处理区"""
-
-        self.timer_sec = self.timer_sec - 1
-        if self.timer_sec == 0 and self.timer_min != 0:
-            self.timer_sec = 60
-            self.timer_min = self.timer_min - 1
-        if self.timer_min == 0 and self.timer_h != 0:
-            self.timer_min = 60
-            self.timer_h = self.timer_h - 1
-        if self.timer_h == 0 and self.timer_min == 0 and self.timer_sec == 0:
-            pass
-
-        if self.timer_sec < 10:
-            self.show_timer_sec = f"0{self.timer_sec}"
-
-        if self.timer_min < 10:
-            self.show_timer_min = f"0{self.timer_min}"
-
-        if self.timer_h < 10:
-            self.show_timer_h = f"0{self.timer_h}"
-
-        self.show_timer_fact_time=f"{self.show_timer_h}:{self.show_timer_min}:{self.timer_h}"
-
-        self.show_timer_fact_time_all.configure(text=self.show_timer_fact_time)
-
-        self.root.after(1000, self._timer_ok_progress)
-
-    def _timer_cancel(self):
-        """在倒计时选择界面退出"""
-
-        # 删除timer下的控件并还原尺寸
-        self.root.geometry("200x100")
-        self.del_timer_choice()
-
-    def del_timer_choice(self):
-        """删除倒计时选择界面的控件"""
-        self.input_s.destroy()
-        self.input_min.destroy()
-        self.input_h.destroy()
-        self.typing_hint.destroy()
-        self.timer_ok.destroy()
-        self.time_cancel.destroy()
-
     def main(self):
         """主程序"""
-        self.root.title("时钟")  # 设置主屏幕标题
+        self.root.title("Clock")  # 设置主屏幕标题
         self.root.geometry("200x100")  # 设置尺寸
         self.root.resizable(False, False)  # 不可缩放
 
         # 定义计时器按钮
-        self.timer_button = Button(text="计时器", command=lambda: self.timer())
+        self.timer_button = Button(text="Timer", command=lambda:self._timer_choose())
         self.timer_button.place(x=20, y=50, height=40, width=80)
         # 定义秒表按钮
-        self.stopwatch = Button(text="秒表")
+        self.stopwatch = Button(text="Stopwatch", command=lambda:self._stopwatch_start())
         self.stopwatch.place(x=100, y=50, height=40, width=80)
 
         self.show_fact_time = tkinter.Label(text="", fg="black", font=("黑体", 40))
@@ -136,6 +39,84 @@ class Clock:
 
         self.root.mainloop()  # 进入循环
 
+    def _timer_choose(self):
+        self.root.geometry("200x140")
+
+        self.choose_h=tkinter.Entry()
+        self.choose_h.place(x=5, y=90, height=30, width=40)
+        self.choose_m=tkinter.Entry()
+        self.choose_m.place(x=50, y=90, height=30, width=40)
+        self.choose_s=tkinter.Entry()
+        self.choose_s.place(x=95, y=90, height=30, width=40)
+        self.choose_hint=tkinter.Label(text="(H/M/S)")
+        self.choose_hint.place(x=45, y=118)
+        self.choose_ok=tkinter.Button(text='OK', command=self._timer_progress)
+        self.choose_ok.place(x=135, y=90, height=45, width=60)
+
+    def _timer_progress(self):
+        self.h=int(self.choose_h.get())
+        self.m=int(self.choose_m.get())
+        self.s=int(self.choose_s.get())
+        
+        
+        self.choose_h.destroy()
+        self.choose_m.destroy()
+        self.choose_s.destroy()
+        self.choose_hint.destroy()
+        self.choose_ok.destroy()
+
+        self._timer_start()
+
+    def _timer_start(self):
+        now=time.time()
+        now+=self.s+self.m*60+self.h*3600
+        timeArray = time.localtime(now)
+        self.checkpoint = time.strftime("%H:%M:%S", timeArray)
+        self._timer_end()
+        
+    def _timer_end(self):
+        time.sleep(self.s+self.m*60+self.h*3600)
+        self.timer_end_hint=tkinter.Label(text="Finish!", font=("黑体", 30))
+        self.timer_end_hint.place(x=15, y=90)
+        self.timer_exit=tkinter.Button(text="Next", command=lambda: self._timer_exit())
+        self.timer_exit.place(x=120, y=90)
+
+    def _timer_exit(self):
+        self.timer_exit.destroy()
+        self.timer_end_hint.destroy()
+        self.root.geometry("200x100")
+
+    def _stopwatch_start(self):
+        self.root.geometry("200x150")
+        self.stopwatch_start_button=tkinter.Button(text="Start", command=lambda:self._stopwatch_timing())
+        self.stopwatch_start_button.place(x=40, y=95, height=50, width=120)
+
+    def _stopwatch_timing(self):
+        self.stopwatch_start_button.destroy()
+        self.start_time=time.time()
+        now=time.strftime("%H:%M:%S")
+        self.stopwatch_start_time=tkinter.Label(text=f"start time:{now}", font=("黑体", 20))
+        self.stopwatch_start_time.place(x=15, y=87)
+        self.stopwatch_end=tkinter.Button(text="END", command=lambda:self._stopwatch_end())
+        self.stopwatch_end.place(x=50, y=115, height=30, width=100)
+    def _stopwatch_end(self):
+        self.stopwatch_end.destroy()
+        self.stopwatch_start_time.destroy()
+        
+        end_time=time.time()
+        time_difference=self.start_time-end_time
+
+        self.stopwatch_time=tkinter.Label(text=abs(time_difference), font=("黑体", 17))
+        self.stopwatch_time.place(x=10,  y=100, height=20)
+
+        self.stopwatch_next=tkinter.Button(text="Next", command=lambda:self._stopwatch_del())
+        self.stopwatch_next.place(x=65, y=120)
+
+    def _stopwatch_del(self):
+        self.root.geometry("200x100")
+        self.stopwatch_next.destroy()
+        self.stopwatch_time.destroy()
+         
 
 if __name__ == "__main__":
     Clock().main()
